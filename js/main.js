bd4644a901a239de33acf26e01c0ae8bab16bc05
Vincenzo <enzo.antignani@gmail.com>
2020-12-11 17:21:42 +0100

    Bozza layout contatti

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ const app = new Vue (
     el: "#root",
     data: {
       contacts: [
+        // Oggetto Michele
       	{
       		name: 'Michele',
       		avatar: 'https://www.jakweb.ch/hd3//package/modern/avatar/5.jpg',
-      		// visible: true,
+      		visible: true,
       		messages: [
       			{
       				date: '10/01/2020 15:30:55',
@@ -25,7 +26,75 @@ const app = new Vue (
       			}
       		],
       	},
-      ],
+
+        //Oggetto Fabio
+      	{
+      		name: 'Fabio',
+      		avatar: 'https://lh3.googleusercontent.com/proxy/2ZfkaB32rbgNMhhf8_dnwate8vpNt3pUnLgLwMvpegkYA9e0klTqmJ-TvZZoYGRosc_psbxFfs5zjQxL6Y6QTz5GejSFOVheGHl2a5ujZjBDUpcIp7PS1h32fdnz9Og',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '20/03/2020 16:30:00',
+      				text: 'Ciao come stai?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '20/03/2020 16:30:55',
+      				text: 'Bene grazie! Stasera ci vediamo?',
+      				status: 'received'
+      			},
+      			{
+      				date: '20/03/2020 16:35:00',
+      				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+      				status: 'sent'
+      			}
+      		],
+      	},
+
+        //Oggetto Samuele
+      	{
+      		name: 'Samuele',
+      		avatar: 'https://www.vanzzptotbv.nl/eigen/img/avatars/s6.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '28/03/2020 10:10:40',
+      				text: 'La Marianna va in campagna',
+      				status: 'received'
+      			},
+      			{
+      				date: '28/03/2020 10:20:10',
+      				text: 'Sicuro di non aver sbagliato chat?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '28/03/2020 16:15:22',
+      				text: 'Ah scusa!',
+      				status: 'received'
+      			}
+      		],
+      	},
+
+        //Oggetto Luisa
+      	{
+      		name: 'Luisa',
+      		avatar: 'https://cdn5.vectorstock.com/i/thumb-large/50/44/woman-portrait-cartoon-avatar-vector-30245044.jpg',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Lo sai che ha aperto una nuova pizzeria?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Si, ma preferirei andare al cinema',
+      				status: 'received'
+      			}
+      		],
+      	},
+      ]
+      //Fine array contacts
     }
 
   }
